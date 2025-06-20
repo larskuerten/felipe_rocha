53a5b5068b9facc071178f5da40df7fc20e48d28
Lars Kuerten <larskuerten@gmail.com>
2025-06-20 12:45:37 -0300

    Botafogo

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -9,7 +9,7 @@ export function TaskPage() {
     const description = searchParams.get("description")
     return (
         <div className="h-screen w-screen bg-slate-500 p-6 flex justify-center">
-            <div className='w-[500px] space-y-4'>
+            <div className='w-[500px] mx-auto space-y-4'>
                 <div className="flex justify-center relative mb-6">
                     <button
                         className="absolute left-0 top-0 bottom-0 text-slate-100"
